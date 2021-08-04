395e540ea16016a0b564e09f1054cee0db2bcdb0
Vladimir Antsupov <antsupov.vova@gmail.com>
2021-08-05 00:05:26 +0300

    logs

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ const STORAGE_DIR = './tmp';
 const PLAYER = 'foobar2000.exe'
 
 
-const getAudio = async (text) => {
-
+const getAudio = async text => {
   const url = googleTTS.getAudioUrl(text, { lang: 'ja-JP' });
   const audio = await axios.get(url, { responseType: 'arraybuffer' });
   const returnedB64 = Buffer.from(audio.data).toString('base64');
-
+  
   const time = Date.now()
-  const fileName = `${time}.mp3`
-
+  const fileName = `${time}_${text.slice(0, 10)}.mp3`
+  
+  console.info(`[${time}]\n${text} \n`);
   fs.writeFileSync(`${STORAGE_DIR}/${fileName}`, returnedB64, { encoding: 'base64' });
   spawn(PLAYER, [`./${STORAGE_DIR}/${fileName}`]);
 }
