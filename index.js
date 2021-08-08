323c4dc6d6fd4ed1b2031b7759c6a6df488f7abc
Vladimir Antsupov <antsupov.vova@gmail.com>
2021-08-08 16:56:17 +0300

    works with tts server

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const googleTTS = require('google-tts-api');
 const clipboardListener = require('clipboard-event');
 const clipboard = require('clipboardy');
 
+const SERVER_URL = 'http://127.0.0.1:5000/audio'
 //should probably use *os.tmpdir()* here
 const STORAGE_DIR = './tmp';
 
@@ -13,14 +14,15 @@ const PLAYER = 'foobar2000.exe'
 
 
 const getAudio = async text => {
-  const url = googleTTS.getAudioUrl(text, { lang: 'ja-JP' });
-  const audio = await axios.get(url, { responseType: 'arraybuffer' });
-  const returnedB64 = Buffer.from(audio.data).toString('base64');
-  
   const time = Date.now()
-  const fileName = `${time}_${text.slice(0, 10)}.mp3`
-  
   console.info(`[${time}]\n${text} \n`);
+
+  console.time()
+  const audio = await axios.post(SERVER_URL, { text }, { responseType: 'arraybuffer' });
+  console.timeEnd()
+  
+  const returnedB64 = Buffer.from(audio.data).toString('base64');
+  const fileName = `${time}_${text.slice(0, 10)}.wav`
   fs.writeFileSync(`${STORAGE_DIR}/${fileName}`, returnedB64, { encoding: 'base64' });
   spawn(PLAYER, [`./${STORAGE_DIR}/${fileName}`]);
 }
