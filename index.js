6fd675bc20d6ed05de9896e58ce7b0391602f31b
Vladimir Antsupov <antsupov.vova@gmail.com>
2021-08-08 20:44:45 +0300

    fixes

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 const fs = require('fs');
 const { spawn } = require('child_process');
-
 const axios = require('axios');
 const clipboardListener = require('clipboard-event');
 const clipboard = require('clipboardy');
 
 //should probably use *os.tmpdir()* here
 const STORAGE_DIR = './tmp';
-const SERVER_URL = 'http://127.0.0.1:5000/audio'
-const PLAYER = 'foobar2000.exe'
+const API_URL = 'http://127.0.0.1:5000/audio'
+const PLAYER_EXECUTABLE = 'foobar2000.exe'
 
 if (!fs.existsSync(STORAGE_DIR)) {
   fs.mkdirSync(STORAGE_DIR);
 }
 
+const sanitizeText = text => {
+  return text
+    .replace('\x00', '')
+}
+
 const getAudio = async text => {
   const time = Date.now()
-  const trimmed_text = text.replace('\x00', '');
-  console.info(`[${time}]\n${trimmed_text}`);
+  const sanitizedText = sanitizeText(text)
+  console.info(`[${time}]\n${sanitizedText}`);
 
   let audio
   try {
-    audio = await axios.post(SERVER_URL, { text: trimmed_text }, { responseType: 'arraybuffer' });
+    audio = await axios.post(API_URL, { text: sanitizedText }, { responseType: 'arraybuffer' });
   } catch {
     console.log('Failed to attain audio from server');
   }
   if (audio) {
-    const fileName = `${time}_${trimmed_text.slice(0, 10)}.wav`
+    const fileName = `${time}_${sanitizedText.slice(0, 15).trim()}.wav`
     fs.writeFileSync(`${STORAGE_DIR}/${fileName}`, audio.data);
-    spawn(PLAYER, [`./${STORAGE_DIR}/${fileName}`]);
+    spawn(PLAYER_EXECUTABLE, [`./${STORAGE_DIR}/${fileName}`]);
   }
 }
 
