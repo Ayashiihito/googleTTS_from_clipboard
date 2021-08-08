840b088eddb45bb92ab3c22e238b2eae15e3187d
Vladimir Antsupov <antsupov.vova@gmail.com>
2021-08-08 18:29:42 +0300

    error handling

--- index.js
+++ index.js
@@ -5,35 +5,45 @@ const axios = require('axios');
 const clipboardListener = require('clipboard-event');
 const clipboard = require('clipboardy');
 
-const SERVER_URL = 'http://127.0.0.1:5000/audio'
 //should probably use *os.tmpdir()* here
 const STORAGE_DIR = './tmp';
-
+const SERVER_URL = 'http://127.0.0.1:5000/audio'
 const PLAYER = 'foobar2000.exe'
 
+if (!fs.existsSync(STORAGE_DIR)) {
+  fs.mkdirSync(STORAGE_DIR);
+}
 
 const getAudio = async text => {
   const time = Date.now()
   console.info(`[${time}]\n${text} \n`);
 
-  console.time()
-  const audio = await axios.post(SERVER_URL, { text }, { responseType: 'arraybuffer' });
-  console.timeEnd()
-  
-  const returnedB64 = Buffer.from(audio.data).toString('base64');
-  const fileName = `${time}_${text.slice(0, 10)}.wav`
-  fs.writeFileSync(`${STORAGE_DIR}/${fileName}`, returnedB64, { encoding: 'base64' });
-  spawn(PLAYER, [`./${STORAGE_DIR}/${fileName}`]);
+  console.time('Time elapsed')
+  let audio
+  try {
+    audio = await axios.post(SERVER_URL, { text }, { responseType: 'arraybuffer' });
+  } catch {
+    console.log('Failed to attain audio from server');
+  }
+  console.timeEnd('Time elapsed')
+  if (audio) {
+    const returnedB64 = Buffer.from(audio.data).toString('base64');
+    const fileName = `${time}_${text.slice(0, 10)}.wav`
+    fs.writeFileSync(`${STORAGE_DIR}/${fileName}`, returnedB64, { encoding: 'base64' });
+    spawn(PLAYER, [`./${STORAGE_DIR}/${fileName}`]);
+  }
 }
 
 clipboardListener.startListening();
-
 clipboardListener.on('change', () => {
-
-  if (!fs.existsSync(STORAGE_DIR)) {
-    fs.mkdirSync(STORAGE_DIR);
+  let text
+  try {
+    text = clipboard.readSync();
+  } catch {
+    console.warn(`Couldn't read clipboard`);
   }
 
-  const text = clipboard.readSync();
   getAudio(text)
 });
+
+console.info('Listening for clipboard change');
