b9b59181755dfc7c3711755dfa0795e91684caa0
Vladimir Antsupov <antsupov.vova@gmail.com>
2021-08-12 21:56:01 +0300

    caching

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const Cache = require("file-system-cache").default
 const { spawn } = require('child_process');
 const axios = require('axios');
 const clipboardListener = require('clipboard-event');
@@ -8,6 +9,11 @@ const clipboard = require('clipboardy');
 const STORAGE_DIR = './tmp';
 const API_URL = 'http://127.0.0.1:5000/audio'
 const PLAYER_EXECUTABLE = 'foobar2000.exe'
+
+const cache = Cache({
+  basePath: STORAGE_DIR, // Optional. Path where cache files are stored (default).
+  ns: "audio" // Optional. A grouping namespace for items.
+});
 
 if (!fs.existsSync(STORAGE_DIR)) {
   fs.mkdirSync(STORAGE_DIR);
@@ -22,17 +28,24 @@ const getAudio = async text => {
   const time = Date.now()
   const sanitizedText = sanitizeText(text)
   console.info(`[${time}]\n${sanitizedText}`);
+  const cachedPath = await cache.get(text)
 
   let audio
-  try {
-    audio = await axios.post(API_URL, { text: sanitizedText }, { responseType: 'arraybuffer' });
-  } catch {
-    console.log('Failed to attain audio from server');
-  }
-  if (audio) {
-    const fileName = `${time}_${sanitizedText.slice(0, 15).trim()}.wav`
-    fs.writeFileSync(`${STORAGE_DIR}/${fileName}`, audio.data);
-    spawn(PLAYER_EXECUTABLE, [`./${STORAGE_DIR}/${fileName}`]);
+  if (!cachedPath) {
+    try {
+      audio = await axios.post(API_URL, { text: sanitizedText }, { responseType: 'arraybuffer' });
+    } catch {
+      console.log('Failed to attain audio from server');
+    }
+    
+    if (audio) {
+      const fileName = `${time}.wav`
+      fs.writeFileSync(`${STORAGE_DIR}/${fileName}`, audio.data);
+      await cache.set(text, fileName)
+      spawn(PLAYER_EXECUTABLE, [`./${STORAGE_DIR}/${fileName}`]);
+    }
+  } else {
+    spawn(PLAYER_EXECUTABLE, [`./${STORAGE_DIR}/${cachedPath}`]);
   }
 }
 
